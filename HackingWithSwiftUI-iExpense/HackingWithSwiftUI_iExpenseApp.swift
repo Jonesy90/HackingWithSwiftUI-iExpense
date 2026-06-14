a42eeb1075ea1ae07b77d269d4289d0d6f3ea82a
Michael Jones <michael.jones90@me.com>
2026-06-14 11:53:04 +0100

    Initial Commit

---
+++ HackingWithSwiftUI-iExpense/HackingWithSwiftUI_iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackingWithSwiftUI_iExpenseApp.swift
+//  HackingWithSwiftUI-iExpense
+//
+//  Created by Michael Jones on 14/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct HackingWithSwiftUI_iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
