7416229d21ccc02bed0f23dad157b233a14287a6
Michael Jones <michael.jones90@me.com>
2026-06-15 17:19:06 +0100

    - AddView: This SwiftUI View is to allow the user to publish a new ExpenseItem and append it to the Expenses class. - ContentView: The body is a List showing all the Expenses that are currently uploaded by the user. - ExpenseItem Struct: The key dimensions and metrics for an Expense item. - Expenses Class: An array of ExpenseItems that saves and reads from UserDefaults.

---
+++ HackingWithSwiftUI-iExpense/AddView.swift
@@ -0,0 +1,49 @@
+//
+//  AddView.swift
+//  HackingWithSwiftUI-iExpense
+//
+//  Created by Michael Jones on 15/06/2026.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.dismiss) var dismiss
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = 0.0
+    
+    var expenses: Expenses
+    
+    var types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(types, id: \.self) { type in
+                        Text(type)
+                    }
+                }
+                
+                TextField("Amount", value: $amount, format: .currency(code: "GBP"))
+                    .keyboardType(.decimalPad)
+            }
+            .navigationTitle("Add New Expense")
+            .toolbar {
+                Button("Save") {
+                    let newExpenseItem = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(newExpenseItem)
+                    dismiss()
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    AddView(expenses: Expenses())
+}
--- HackingWithSwiftUI-iExpense/ContentView.swift
+++ HackingWithSwiftUI-iExpense/ContentView.swift
@@ -7,15 +7,77 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+struct ExpenseItem: Identifiable, Codable {
+    var id = UUID()
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+@Observable
+class Expenses {
+    var items = [ExpenseItem]() {
+        didSet {
+            if let encoded = try? JSONEncoder().encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "Items")
+            }
         }
-        .padding()
+    }
+    
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        
+        items = []
+    }
+}
+
+struct ContentView: View {
+    @State private var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
+    var body: some View {
+        NavigationStack {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        
+                        Spacer()
+                        
+                        HStack {
+                            Text(item.amount, format: .currency(code: "GBP"))
+                        }
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationTitle("iExpense")
+            
+            .toolbar {
+                Button("Add Expense", systemImage: "plus") {
+                    showingAddExpense = true
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
+            
+        }
+    }
+    
+    //Purpose: Designed to remove one or more items from the 'expenses.item' array at the specified positions.
+    //How it works?: The parameter represents a set of indexes that should be removes. With the convenience method (provided in Swift for arrays), it will remove all elements in the array at the indexes specified.
+    private func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
